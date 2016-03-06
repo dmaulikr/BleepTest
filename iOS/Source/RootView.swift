2e7a653a4d2c7e32e7dd50a703cf4e966f86a6e5
Ieuan Tudur Peace <ieuan@semanties.com>
2016-03-06 17:21:48 +0000

    Created the Stop button

--- iOS/Source/RootView.swift
+++ iOS/Source/RootView.swift
@@ -1,10 +1,25 @@
 import UIKit
 
 class RootView: UIView {
+    
+    lazy var startButton: UIButton = {
+        var temporyButton : UIButton = UIButton()
+        temporyButton.frame = CGRect(x: 0, y: 0, width: 200, height: 50)
+        temporyButton.center = CGPoint(x: 160, y: 100)
+        temporyButton.setTitle("Stop", forState: UIControlState.Normal)
+        temporyButton.setTitleColor(.redColor(), forState: UIControlState.Normal)
+        temporyButton.backgroundColor = .clearColor()
+        temporyButton.layer.cornerRadius = 5
+        temporyButton.layer.borderWidth = 1
+        temporyButton.layer.borderColor = UIColor.redColor().CGColor
+        return temporyButton
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-
-       // backgroundColor = .redColor()
+        
+        addSubview(startButton)
+        backgroundColor = .whiteColor()
     }
 
     required init?(coder aDecoder: NSCoder) {
