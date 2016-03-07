03a94e57d60286d0e8f7d056e6dec3f9fea8bae8
Ieuan Tudur Peace <ieuan@semanties.com>
2016-03-07 18:43:56 +0000

    Hiding and showing the start and stop/pause buttons when pressed

--- iOS/Source/BleepTestView.swift
+++ iOS/Source/BleepTestView.swift
@@ -2,7 +2,6 @@ import UIKit
 
 class BleepTestView: UIView {
     
-    let buttonSize = CGSize(width: 200, height: 50)
     let cornerRadius : CGFloat = 5
     let borderWidth : CGFloat = 2
     
@@ -10,6 +9,7 @@ class BleepTestView: UIView {
         var temporyButton : UIButton = UIButton()
         temporyButton.setTitle("Stop", forState: UIControlState.Normal)
         temporyButton.setTitleColor(.redColor(), forState: UIControlState.Normal)
+        temporyButton.addTarget(self, action: "stopButtonAction:", forControlEvents: UIControlEvents.TouchUpInside)
         temporyButton.backgroundColor = .clearColor()
         temporyButton.layer.cornerRadius = self.cornerRadius
         temporyButton.layer.borderWidth = self.borderWidth
@@ -22,6 +22,7 @@ class BleepTestView: UIView {
         var temporyButton : UIButton = UIButton()
         temporyButton.setTitle("Start", forState: UIControlState.Normal)
         temporyButton.setTitleColor(.greenColor(), forState: UIControlState.Normal)
+        temporyButton.addTarget(self, action: "startButtonAction:", forControlEvents: UIControlEvents.TouchUpInside)
         temporyButton.backgroundColor = .clearColor()
         temporyButton.layer.cornerRadius = self.cornerRadius
         temporyButton.layer.borderWidth = self.borderWidth
@@ -29,6 +30,19 @@ class BleepTestView: UIView {
         temporyButton.translatesAutoresizingMaskIntoConstraints = false
         return temporyButton
     }()
+    
+    lazy var pauseButton : UIButton = {
+        var temporyButton : UIButton = UIButton()
+        temporyButton.setTitle("Pause", forState: UIControlState.Normal)
+        temporyButton.setTitleColor(.blueColor(), forState: UIControlState.Normal)
+        temporyButton.backgroundColor = .clearColor()
+        temporyButton.layer.cornerRadius = self.cornerRadius
+        temporyButton.layer.borderWidth = self.borderWidth
+        temporyButton.layer.borderColor = UIColor.blueColor().CGColor
+        temporyButton.translatesAutoresizingMaskIntoConstraints = false
+        return temporyButton
+    }()
+
     
     lazy var stageLabel : UILabel = {
         var temporyLabel : UILabel = UILabel()
@@ -38,55 +52,86 @@ class BleepTestView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        addSubview(stopButton)
-        addSubview(startButton)
-        creatingConstraints()
+        
+        createStartBleepTest()
         backgroundColor = .whiteColor()
-    }
-    
-    func creatingConstraints(){
-        //make dictionary for buttons
-        let viewsDictionary = [
-            "stopButton":stopButton,
-            "startButton":startButton,
-            "superview":self
-        ]
-        
-        //Position constraints
-        let centerHorizontalyConstraint = NSLayoutConstraint.constraintsWithVisualFormat(
-            "V:[superview]-(<=1)-[stopButton]",
-            options: NSLayoutFormatOptions.AlignAllCenterX,
-            metrics: nil,
-            views: viewsDictionary
-        )
-        let verticalConstraints = NSLayoutConstraint.constraintsWithVisualFormat(
-            "V:|-50-[stopButton(50)]-(>=300)-[startButton(50)]-50-|",
-            options: NSLayoutFormatOptions.AlignAllLeading,
-            metrics: nil,
-            views: viewsDictionary
-        )
-        let stopButtonHorizontalSizeConstraints = NSLayoutConstraint.constraintsWithVisualFormat(
-            "H:[stopButton(200)]",
-            options: NSLayoutFormatOptions.AlignAllLeading,
-            metrics: nil,
-            views: viewsDictionary
-        )
-        let startButtonHorizontalSizeConstraints = NSLayoutConstraint.constraintsWithVisualFormat(
-            "H:[startButton(200)]",
-            options: NSLayoutFormatOptions.AlignAllLeading,
-            metrics: nil,
-            views: viewsDictionary
-        )
-        
-        self.addConstraints(centerHorizontalyConstraint)
-        self.addConstraints(verticalConstraints)
-        self.addConstraints(stopButtonHorizontalSizeConstraints)
-        self.addConstraints(startButtonHorizontalSizeConstraints)
-
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    //This is the view when the bleep test is running
+    func createRunningBleepTest(){
+        let viewsDictionary = [
+            "stopButton":stopButton,
+            "pauseButton":pauseButton,
+            "superview":self
+        ]
+        addSubview(stopButton)
+        addSubview(pauseButton)
+        self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:[superview]-(<=1)-[stopButton]",
+            options: NSLayoutFormatOptions.AlignAllCenterX,
+            metrics: nil,
+            views: viewsDictionary
+            ))
+        self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:|-(>=50)-[stopButton(50)]-(>=20)-[pauseButton(50)]-50-|",
+            options: NSLayoutFormatOptions.AlignAllLeading,
+            metrics: nil,
+            views: viewsDictionary))
+        self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "H:[stopButton(200)]",
+            options: NSLayoutFormatOptions.AlignAllLeading,
+            metrics: nil,
+            views: viewsDictionary
+            ))
+
+        self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "H:[pauseButton(200)]",
+            options: NSLayoutFormatOptions.AlignAllLeading,
+            metrics: nil,
+            views: viewsDictionary
+            ))
+    }
+    
+    //Inital view when the bleep test hasent started
+    func createStartBleepTest(){
+        let viewsDictionary = [
+            "startButton":startButton,
+            "superview":self
+        ]
+        addSubview(startButton)
+        self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:[superview]-(<=1)-[startButton]",
+            options: NSLayoutFormatOptions.AlignAllCenterX,
+            metrics: nil,
+            views: viewsDictionary
+            ))
+        self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:|-(>=20)-[startButton(50)]-50-|",
+            options: NSLayoutFormatOptions.AlignAllLeading,
+            metrics: nil,
+            views: viewsDictionary
+            ))
+        self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "H:[startButton(200)]",
+            options: NSLayoutFormatOptions.AlignAllLeading,
+            metrics: nil,
+            views: viewsDictionary
+            ))
+    }
+    
+    func startButtonAction(sender:UIButton!){
+        startButton.removeFromSuperview()
+        createRunningBleepTest()
+    }
+    
+    func stopButtonAction(sender:UIButton!){
+        stopButton.removeFromSuperview()
+        pauseButton.removeFromSuperview()
+        createStartBleepTest()
     }
 
 }
