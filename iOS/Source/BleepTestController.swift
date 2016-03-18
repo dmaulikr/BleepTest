b33bdf1500bc75c5bbf8ca71f15856d82a705a93
Ieuan Tudur Peace <ieuan@semantise.com>
2016-03-18 15:55:14 +0000

    fixing the calculation

--- iOS/Source/BleepTestController.swift
+++ iOS/Source/BleepTestController.swift
@@ -49,7 +49,7 @@ class BleepTestController: BaseViewController {
         if (i != levels.count){
             testLevel = levels[i]
             lap = 0
-            let vO2Max = 3.46 * (Double(testLevel.level)+Double(lap) / (Double(testLevel.level) * 0.4325 + 7.0048)) + 12.2
+            let vO2Max = 3.46 * (Double(testLevel.level)+Double(lap+1) / ((Double(testLevel.level) * 0.4325 + 7.0048))) + 12.2
             NSNotificationCenter.defaultCenter().postNotificationName(
                 leveledUpNotificationKey,
                 object: nil,
@@ -104,7 +104,7 @@ class BleepTestController: BaseViewController {
     private func lapFinished(){
         lap = lap + 1
         distance = distance + 20
-        let vO2Max = 3.46 * (Double(level+lap) / (Double(level) * 0.4325 + 7.0048)) + 12.2
+        let vO2Max = 3.46 * (Double(testLevel.level)+Double(lap+1) / ((Double(testLevel.level) * 0.4325 + 7.0048))) + 12.2
         NSNotificationCenter.defaultCenter().postNotificationName(
             lapedUpNotificationKey,
             object: nil,
