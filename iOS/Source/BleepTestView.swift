19ec12d369f02a49e0e5a09f7f88de1c2f040342
Ieuan Tudur Peace <ieuan@semantise.com>
2016-03-20 18:21:42 +0000

    Removed the pause button from the view.
    Don’t think I need the pause button, might change my mind

--- iOS/Source/BleepTestView.swift
+++ iOS/Source/BleepTestView.swift
@@ -30,18 +30,6 @@ class BleepTestView: UIView {
         temporyButton.translatesAutoresizingMaskIntoConstraints = false
         return temporyButton
     }()
-    
-    lazy var pauseButton : UIButton = {
-        var temporyButton : UIButton = UIButton()
-        temporyButton.setTitle("Pause", forState: UIControlState.Normal)
-        temporyButton.setTitleColor(.blueColor(), forState: UIControlState.Normal)
-        temporyButton.backgroundColor = .clearColor()
-        temporyButton.layer.cornerRadius = self.cornerRadius
-        temporyButton.layer.borderWidth = self.borderWidth
-        temporyButton.layer.borderColor = UIColor.blueColor().CGColor
-        temporyButton.translatesAutoresizingMaskIntoConstraints = false
-        return temporyButton
-    }()
 
     lazy var levelLabel : UILabel = {
         var temporyLabel : UILabel = UILabel()
@@ -135,11 +123,9 @@ class BleepTestView: UIView {
             "levelLabel":levelLabel,
             "lapLabel":lapLabel,
             "stopButton":stopButton,
-            "pauseButton":pauseButton,
             "superview":self
         ]
         addSubview(stopButton)
-        addSubview(pauseButton)
         addSubview(levelLabel)
         addSubview(lapLabel)
         self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
@@ -149,18 +135,12 @@ class BleepTestView: UIView {
             views: viewsDictionary
             ))
         self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
-            "V:|-(>=50)-[stopButton(50)]-20-[pauseButton(50)]-50-|",
+            "V:|-(>=50)-[stopButton(50)]-120-|",
             options: NSLayoutFormatOptions.AlignAllLeading,
             metrics: nil,
             views: viewsDictionary))
         self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
             "H:[stopButton(200)]",
-            options: NSLayoutFormatOptions.AlignAllLeading,
-            metrics: nil,
-            views: viewsDictionary
-            ))
-        self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
-            "H:[pauseButton(200)]",
             options: NSLayoutFormatOptions.AlignAllLeading,
             metrics: nil,
             views: viewsDictionary
@@ -207,7 +187,6 @@ class BleepTestView: UIView {
     
     func stopButtonAction(sender:UIButton!){
         stopButton.removeFromSuperview()
-        pauseButton.removeFromSuperview()
         createStartBleepTest()
         NSNotificationCenter.defaultCenter().postNotificationName(
             stopTestNotificationKey,
@@ -248,7 +227,6 @@ class BleepTestView: UIView {
     
     func bleepTestStoped(notification : NSNotification){
         stopButton.removeFromSuperview()
-        pauseButton.removeFromSuperview()
         createStartBleepTest()
     }
 
