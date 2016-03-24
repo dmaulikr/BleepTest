b49a18185261d552719e273f22a430bd09f6f244
Ieuan Tudur Peace <ieuan@semantise.com>
2016-03-24 14:15:16 +0000

    Creating the tab bar controller

---
+++ Library/TabBarController.swift
@@ -0,0 +1,21 @@
+//
+//  TabBarController.swift
+//  Project
+//
+//  Created by Ieuan Peace on 24/03/2016.
+//
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
