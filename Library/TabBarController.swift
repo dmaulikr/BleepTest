f4e9c65e012e63333c2710b6f3b7f18af34cb88b
Ieuan Tudur Peace <ieuan@semantise.com>
2016-03-24 16:16:34 +0000

    Add the tab bar item's

--- Library/TabBarController.swift
+++ Library/TabBarController.swift
@@ -9,13 +9,58 @@
 import UIKit
 
 class TabBarController: UITabBarController {
+    internal var fetcher: Fetcher
+    
+    // MARK: - Initializers
+    
+     init(fetcher: Fetcher) {
+        self.fetcher = fetcher
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+     required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
 
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
+
+    override func viewWillAppear(animated: Bool) {
+        
+        let item1 = BleepTestController(fetcher: self.fetcher)
+        let icon1 = UITabBarItem(title: "Bleep Test", image:nil, selectedImage:nil)
+        item1.tabBarItem = icon1
+        let navigationController1 = UINavigationController(rootViewController: item1)
+        navigationController1.navigationBar.barTintColor = .customBlueColor()
+        navigationController1.navigationBar.tintColor = .whiteColor()
+        navigationController1.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+
+        
+        let storyboard = UIStoryboard(name: "CompletedTests", bundle: nil)
+        let item2 = storyboard.instantiateViewControllerWithIdentifier("viewController")
+        let icon2 = UITabBarItem(title: "My stats", image:nil, selectedImage:nil)
+        item2.tabBarItem = icon2
+        let navigationController2 = UINavigationController(rootViewController: item2)
+        navigationController2.navigationBar.barTintColor = .customBlueColor()
+        navigationController2.navigationBar.tintColor = .whiteColor()
+        navigationController2.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+
+        let controllers = [navigationController1, navigationController2]
+        self.viewControllers = controllers
+    }
+}
+
+extension TabBarController{
+    //MARK: UITabBarControllerDelegate
+    func tabBarController(tabBarController: UITabBarController, shouldSelectViewController viewController: UIViewController) -> Bool {
+        print("Should select viewController: \(viewController.title) ?")
+        return true;
+    }
 }
