0557033aa9bc3331553fd9736713ae3ac6a2ee6d
Ieuan Tudur Peace <ieuan@semantise.com>
2016-04-03 14:32:34 +0100

    Home classes

---
+++ iOS/Source/HomeController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeController.swift
+//  Project
+//
+//  Created by Ieuan Peace on 03/04/2016.
+//
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ iOS/Source/HomeView.swift
@@ -0,0 +1,15 @@
+//
+//  HomeView.swift
+//  Project
+//
+//  Created by Ieuan Peace on 03/04/2016.
+//
+//
+
+import UIKit
+
+class HomeView: UIView {
+
+
+
+}
