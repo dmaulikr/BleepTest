866e7302db4b3b6eb427d023113a40ebe2168bfb
Ieuan Tudur Peace <ieuan@semantise.com>
2016-04-16 15:58:36 +0100

    MediumTitleLabel label

---
+++ Library/Subclasses/Labels/MediumTitleLabel.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class MediumTitleLabel: UILabel {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.font = UIFont(name: self.font.fontName, size: 23)
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.textColor = UIColor.grayColor()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
