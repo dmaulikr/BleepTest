fbeea4bb1e4f95c17871349b84359504a7347090
Ieuan Tudur Peace <ieuan@semantise.com>
2016-09-18 21:42:03 +0100

    Remove the layout for adding the team
    Did not like the layout for adding team - going to replace it.

--- iOS/Source/HomeView.swift
+++ iOS/Source/HomeView.swift
@@ -2,37 +2,6 @@ import UIKit
 
 class HomeView: UIView {
     
-    lazy var typeOfBleepTestSegment : UISegmentedControl = {
-        let items = ["Single", "Team"]
-        let segmentedControl = UISegmentedControl(items: items)
-        let font = UIFont.systemFontOfSize(18)
-        segmentedControl.setTitleTextAttributes([NSFontAttributeName: font], forState: UIControlState.Normal)
-        segmentedControl.addTarget(self, action: #selector(typeSegment(_:)), forControlEvents: UIControlEvents.ValueChanged)
-        segmentedControl.selectedSegmentIndex = 0
-        segmentedControl.layer.cornerRadius = 5.0
-        segmentedControl.tintColor = UIColor.italyBrownColor()
-        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
-        return segmentedControl
-    }()
-    
-    lazy var lapDistanceTitleLabel : MediumTitleLabel = {
-        var temporyLabel : MediumTitleLabel = MediumTitleLabel()
-        temporyLabel.text = "Lap distance"
-        return temporyLabel
-    }()
-    
-    lazy var lapDistanceSegment : UISegmentedControl = {
-        let items = ["15m", "20m"]
-        let segmentedControl = UISegmentedControl(items: items)
-        let font = UIFont.systemFontOfSize(18)
-        segmentedControl.setTitleTextAttributes([NSFontAttributeName: font], forState: UIControlState.Normal)
-        segmentedControl.selectedSegmentIndex = 1
-        segmentedControl.layer.cornerRadius = 5.0
-        segmentedControl.tintColor = UIColor.italyBrownColor()
-        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
-        return segmentedControl
-    }()
-    
     lazy var startButton : StartButton = {
         var temporyButton : StartButton = StartButton()
         temporyButton.addTarget(self, action: #selector(startButtonAction(_:)), forControlEvents: UIControlEvents.TouchUpInside)
@@ -51,55 +20,22 @@ class HomeView: UIView {
     
     func addUIComponentsToView(){
         let viewsDictionary = [
-            "lapDistanceTitleLabel":lapDistanceTitleLabel,
-            "lapDistanceSegment":lapDistanceSegment,
             "startButton":startButton,
-            "typeOfBleepTestSegment":typeOfBleepTestSegment,
             "superview":self
         ]
-        addSubview(typeOfBleepTestSegment)
-        addSubview(lapDistanceTitleLabel)
-        addSubview(lapDistanceSegment)
         addSubview(startButton)
         self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
-            "V:[superview]-(<=1)-[startButton]",
+            "V:[superview]-(<=1)-[startButton(100)]",
             options: NSLayoutFormatOptions.AlignAllCenterX,
             metrics: nil,
             views: viewsDictionary
             ))
         self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
-            "V:|-(>=50)-[startButton(100)]-80-|",
-            options: NSLayoutFormatOptions.AlignAllCenterX,
-            metrics: nil,
-            views: viewsDictionary))
-        self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
-            "V:|-(>=50)-[lapDistanceSegment(35)]-260-|",
-            options: NSLayoutFormatOptions.AlignAllCenterX,
-            metrics: nil,
-            views: viewsDictionary))
-        self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
-            "H:[startButton(100)]",
-            options: NSLayoutFormatOptions.AlignAllLeading,
-            metrics: nil,
-            views: viewsDictionary
-            ))
-        self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
-            "V:|-80-[typeOfBleepTestSegment(40)]-(>=300)-|",
-            options: NSLayoutFormatOptions.AlignAllCenterX,
-            metrics: nil,
-            views: viewsDictionary))
-        self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
-            "H:|-10-[typeOfBleepTestSegment(>=280)]-10-|",
-            options: NSLayoutFormatOptions.AlignAllLeading,
-            metrics: nil,
-            views: viewsDictionary
-            ))
-        self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
-            "H:|-20-[lapDistanceTitleLabel]-(>=20)-[lapDistanceSegment(100)]-20-|",
+            "H:[superview]-(<=1)-[startButton(100)]",
             options: NSLayoutFormatOptions.AlignAllCenterY,
             metrics: nil,
-            views: viewsDictionary
-            ))
+            views: viewsDictionary))
+
     }
     
     func singleBleepTestView(){
@@ -116,16 +52,5 @@ extension HomeView{
         NSNotificationCenter.defaultCenter().postNotificationName(
             startTestNotificationKey,
             object: self)
-    }
-    
-    func typeSegment(sender:UISegmentedControl!){
-        NSNotificationCenter.defaultCenter().postNotificationName(
-            typeChangedNotificationKey,
-            object: self)
-        if(typeOfBleepTestSegment.selectedSegmentIndex == 0){
-            singleBleepTestView()
-        } else{
-            teamBleepTestView()
-        }
     }
 }
