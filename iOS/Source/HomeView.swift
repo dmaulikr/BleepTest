db234b7c50758eef56dd10eed5b5ac3d38b14857
Ieuan Tudur Peace <ieuan@semantise.com>
2016-09-18 22:31:42 +0100

    Re-add the team or single UISegmentController

--- iOS/Source/HomeView.swift
+++ iOS/Source/HomeView.swift
@@ -2,6 +2,19 @@ import UIKit
 
 class HomeView: UIView {
     
+    lazy var typeOfBleepTestSegment : UISegmentedControl = {
+        let items = ["Single", "Team"]
+        let segmentedControl = UISegmentedControl(items: items)
+        let font = UIFont.systemFontOfSize(18)
+        segmentedControl.setTitleTextAttributes([NSFontAttributeName: font], forState: UIControlState.Normal)
+        segmentedControl.addTarget(self, action: #selector(typeSegment(_:)), forControlEvents: UIControlEvents.ValueChanged)
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.layer.cornerRadius = 5.0
+        segmentedControl.tintColor = UIColor.blueColor()
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        return segmentedControl
+    }()
+    
     lazy var startButton : StartButton = {
         var temporyButton : StartButton = StartButton()
         temporyButton.addTarget(self, action: #selector(startButtonAction(_:)), forControlEvents: UIControlEvents.TouchUpInside)
@@ -20,10 +33,25 @@ class HomeView: UIView {
     
     func addUIComponentsToView(){
         let viewsDictionary = [
+            "typeOfBleepTestSegment":typeOfBleepTestSegment,
             "startButton":startButton,
             "superview":self
         ]
+        
         addSubview(startButton)
+        addSubview(typeOfBleepTestSegment)
+        
+        self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:|-120-[typeOfBleepTestSegment(40)]-(>=300)-|",
+            options: NSLayoutFormatOptions.AlignAllCenterX,
+            metrics: nil,
+            views: viewsDictionary))
+        self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "H:|-20-[typeOfBleepTestSegment(>=280)]-20-|",
+            options: NSLayoutFormatOptions.AlignAllLeading,
+            metrics: nil,
+            views: viewsDictionary))
+
         self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
             "V:[superview]-(<=1)-[startButton(100)]",
             options: NSLayoutFormatOptions.AlignAllCenterX,
@@ -42,9 +70,21 @@ class HomeView: UIView {
             views: viewsDictionary))
     }
     
+    func typeSegment(sender:UISegmentedControl!){
+        NSNotificationCenter.defaultCenter().postNotificationName(
+            typeChangedNotificationKey,
+            object: self)
+        if(typeOfBleepTestSegment.selectedSegmentIndex == 0){
+            singleBleepTestView()
+        } else{
+            teamBleepTestView()
+        }
+    }
+    
     func singleBleepTestView(){
         print("single")
     }
+    
     func teamBleepTestView(){
         print("team")
     }
