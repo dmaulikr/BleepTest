820e0c1f9e94ef3715ca1618052f620bc4633153
Ieuan Tudur Peace <ieuan@semantise.com>
2016-09-24 21:11:14 +0100

    Changing the colour a bit

--- Library/Extentions/UIColor+Custom.swift
+++ Library/Extentions/UIColor+Custom.swift
@@ -6,13 +6,19 @@ extension UIColor {
         return UIColor(hex: "FF4B47")
     }
     class func italyGreenColor() -> UIColor {
-        return UIColor(hex: "#009246")
+        return UIColor(hex: "#86c06d")
     }
     class func customBlueColor() -> UIColor {
-        return UIColor(hex: "#4A90E2")
+        return UIColor(hex: "#4a86e8")
     }
     class func customLightBlueColor() -> UIColor {
-        return UIColor(hex: "#87a6fd")
+        return UIColor(hex: "#72c8ed")
+    }
+    class func pinkRedColor() -> UIColor {
+        return UIColor(hex: "#e06666")
+    }
+    class func customRedColor() -> UIColor {
+        return UIColor(hex: "#cc0000")
     }
     class func italyBrownColor() -> UIColor {
         return UIColor(hex :"#653606")
