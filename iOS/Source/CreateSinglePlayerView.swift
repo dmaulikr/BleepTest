84b82be1ed02de6a17ddb7e073ac7c5c874ba82b
Ieuan Tudur Peace <ieuan@semantise.com>
2016-10-02 23:59:10 +0100

    Add the Username text field to the view

--- iOS/Source/CreateSinglePlayerView.swift
+++ iOS/Source/CreateSinglePlayerView.swift
@@ -2,5 +2,46 @@ import UIKit
 
 class CreateSinglePlayerView: UIView {
 
+    lazy var userNameTextField: YoshikoTextField = {
+        let temporyTextField: YoshikoTextField = YoshikoTextField(frame: CGRect(x: 0, y: 0, width: 250, height: 55))
+        temporyTextField.placeholder = "Username"
+        return temporyTextField
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        createView()
+        backgroundColor = .whiteColor()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
+    func createView(){
+        let viewsDictionary = [
+            "userNameTextField": userNameTextField,
+            "superview": self
+        ]
+
+        addSubview(userNameTextField)
+        
+        self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:|-50-[userNameTextField(55)]-(>=20)-|",
+            options: NSLayoutFormatOptions.AlignAllCenterX,
+            metrics: nil,
+            views: viewsDictionary))
+        self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:[superview]-(<=1)-[userNameTextField]",
+            options: NSLayoutFormatOptions.AlignAllCenterX,
+            metrics: nil,
+            views: viewsDictionary))
+        self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "H:[userNameTextField(250)]",
+            options: NSLayoutFormatOptions.AlignAllCenterX,
+            metrics: nil,
+            views: viewsDictionary))
+        
+    }
+    
 }
