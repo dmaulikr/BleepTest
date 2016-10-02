8bd538749cf3e63331b14c1e72205aff4f0f401c
Ieuan Tudur Peace <ieuan@semantise.com>
2016-10-02 23:51:52 +0100

    Colours for the custom text fields

--- Library/Subclasses/TextFields/YoshikoTextField.swift
+++ Library/Subclasses/TextFields/YoshikoTextField.swift
@@ -170,6 +170,17 @@ import UIKit
     }
     
     // MARK: - Overrides
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.inactiveBorderColor = .lightGrayColor()
+        self.activeBorderColor = UIColor.pinkRedColor()
+        self.placeholderColor = UIColor.whiteColor()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override public var bounds: CGRect {
         didSet {
