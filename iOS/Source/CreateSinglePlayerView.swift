447cf454cadfeb444849c90e01440a1b65a1384c
Ieuan Tudur Peace <ieuan@semantise.com>
2016-10-04 22:04:15 +0100

    Age text field

--- iOS/Source/CreateSinglePlayerView.swift
+++ iOS/Source/CreateSinglePlayerView.swift
@@ -4,7 +4,12 @@ class CreateSinglePlayerView: UIView {
 
     lazy var userNameTextField: YoshikoTextField = {
         let temporyTextField: YoshikoTextField = YoshikoTextField(frame: CGRect(x: 0, y: 0, width: 250, height: 55))
-        temporyTextField.placeholder = "Username"
+        return temporyTextField
+    }()
+    
+    lazy var ageTextField: YoshikoTextField = {
+        let temporyTextField: YoshikoTextField = YoshikoTextField(frame: CGRect(x: 0, y: 0, width: 250, height: 55))
+        temporyTextField.placeholder = "Age   eg 27"
         return temporyTextField
     }()
     
@@ -21,23 +26,35 @@ class CreateSinglePlayerView: UIView {
     func createView(){
         let viewsDictionary = [
             "userNameTextField": userNameTextField,
+            "ageTextField": ageTextField,
             "superview": self
         ]
 
         addSubview(userNameTextField)
+        addSubview(ageTextField)
         
         self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
-            "V:|-50-[userNameTextField(55)]-(>=20)-|",
+            "V:|-100-[userNameTextField(55)]-30-[ageTextField(55)]-(>=20)-|",
             options: NSLayoutFormatOptions.AlignAllCenterX,
             metrics: nil,
             views: viewsDictionary))
         self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
-            "V:[superview]-(<=1)-[userNameTextField]",
+            "V:[superview]-(<=1)-[userNameTextField(55)]",
             options: NSLayoutFormatOptions.AlignAllCenterX,
             metrics: nil,
             views: viewsDictionary))
         self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
             "H:[userNameTextField(250)]",
+            options: NSLayoutFormatOptions.AlignAllCenterX,
+            metrics: nil,
+            views: viewsDictionary))
+        self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:[superview]-(<=1)-[ageTextField]",
+            options: NSLayoutFormatOptions.AlignAllCenterX,
+            metrics: nil,
+            views: viewsDictionary))
+        self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "H:[ageTextField(250)]",
             options: NSLayoutFormatOptions.AlignAllCenterX,
             metrics: nil,
             views: viewsDictionary))
