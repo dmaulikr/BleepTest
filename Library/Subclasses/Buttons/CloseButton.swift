3e2f2ced2d8068fc11bd739f0727090ba631b42e
Ieuan Tudur Peace <ieuan@semantise.com>
2016-10-08 17:43:18 +0100

    Using Close image in the CloseButton

--- Library/Subclasses/Buttons/CloseButton.swift
+++ Library/Subclasses/Buttons/CloseButton.swift
@@ -1,22 +1,14 @@
 import UIKit
 
 class CloseButton: UIButton {
-    override func drawRect(rect: CGRect) {
-        let path = UIBezierPath(ovalInRect: rect)
-        if(highlighted){
-            UIColor.lightGrayColor().setFill()
-        } else{
-            UIColor.veryLightGrayColor().setFill()
-        }
-        path.fill()
-    }
+
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.setTitle("X", forState:UIControlState.Normal)
-        self.setTitleColor(.whiteColor(), forState: UIControlState.Normal)
-        self.titleLabel?.font = UIFont(name: (self.titleLabel?.font.fontName)!, size: 15)
         self.translatesAutoresizingMaskIntoConstraints = false
+        
+        let closeImage = UIImage(named: "Close") as UIImage?
+        self.setImage(closeImage, forState: UIControlState.Normal)
     }
     
     override var highlighted: Bool {
