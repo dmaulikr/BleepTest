4167f90e7fbbf8302257807853cc617a6ffe7373
Ieuan Tudur Peace <ieuan@semantise.com>
2017-01-01 14:56:38 +0000

    Show UIStatusBar

--- iOS/Source/CreateSinglePlayerController.swift
+++ iOS/Source/CreateSinglePlayerController.swift
@@ -21,6 +21,7 @@ extension CreateSinglePlayerController : CreateSinglePlayerViewDelegate {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     func didCreateButtonPressed(sender: CreateSinglePlayerView, age:Int, userName:NSString){
+        setStatusBarHidden(false)
         writer.createPlayer(userName as String, age:age)
         self.dismissViewControllerAnimated(true, completion: nil)
     }
