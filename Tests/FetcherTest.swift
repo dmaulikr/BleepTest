a318dd16a0ad6f71f1aa78fb06a3d30adf726f10
Ieuan Tudur Peace <ieuan@semantise.com>
2017-01-01 12:33:54 +0000

    Test fetch users

---
+++ Tests/FetcherTest.swift
@@ -0,0 +1,35 @@
+import XCTest
+import DATAStack
+import CoreData
+@testable import BleepTest
+
+class FetcherTest: XCTestCase {
+    
+    func createDataStack() -> DATAStack {
+        let dataStack = DATAStack(modelName: "iOS", bundle: NSBundle.mainBundle(), storeType:.InMemory)
+        return dataStack
+    }
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testFetchUsers() {
+        let data = self.createDataStack()
+        
+        let user = NSEntityDescription.insertNewObjectForEntityForName("Player", inManagedObjectContext: data.mainContext)
+        user.setValue(101, forKey: "age")
+        user.setValue("Billy Jones", forKey: "username")
+        try! data.mainContext.save()
+        
+        let fetcher = Fetcher(dataStack: data)
+        let players = fetcher.fetchUsers{_ in}
+
+        XCTAssertEqual(players.count, 1)
+    }
+    
+}
