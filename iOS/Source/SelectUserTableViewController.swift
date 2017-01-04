063b436775a85fd529c6cb8a45b8d48d19ca06c8
Ieuan Tudur Peace <ieuan@semantise.com>
2017-01-04 18:16:00 +0000

    Showing the Players

--- iOS/Source/SelectUserTableViewController.swift
+++ iOS/Source/SelectUserTableViewController.swift
@@ -1,9 +1,25 @@
 import UIKit
+import DATASource
+import DATAStack
+import CoreData
 
 class SelectUserTableViewController: BaseTableViewController {
 
+    lazy var dataSource: DATASource = {
+        let request = NSFetchRequest(entityName: "Player")
+        request.sortDescriptors = [NSSortDescriptor(key: "dateAdded", ascending: true)]
+        
+        let dataSource = DATASource(tableView: self.tableView, cellIdentifier: "Cell", fetchRequest: request, mainContext: self.dataStack!.mainContext, configuration: { cell, item, indexPath in
+            cell.textLabel?.text = item.valueForKey("username") as? String
+        })
+        return dataSource
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        self.tableView = UITableView(frame: self.tableView.frame, style: .Grouped)
+        self.tableView.dataSource = self.dataSource
     }
     
 }
