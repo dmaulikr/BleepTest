b3864ca6fa3d6dd971933f29d9ec7526a0a3a81d
Ieuan Tudur Peace <ieuan@semantise.com>
2017-01-07 15:53:55 +0000

    Dismiss the keyboard
    - Dismiss when the background is taped
    - Dismiss when leaving dismissing the view controller

--- iOS/Source/CreateSinglePlayerController.swift
+++ iOS/Source/CreateSinglePlayerController.swift
@@ -7,6 +7,7 @@ class CreateSinglePlayerController: BaseViewController {
         self.view = view
         view.delegate = self
         setStatusBarHidden(true)
+        self.hideKeyboardWhenTappedAround()
     }
     
     override func viewDidLoad() {
@@ -17,10 +18,12 @@ class CreateSinglePlayerController: BaseViewController {
 
 extension CreateSinglePlayerController : CreateSinglePlayerViewDelegate {
     func didCloseButtonPressed(sender: CreateSinglePlayerView) {
+        view.endEditing(true)
         setStatusBarHidden(false)
         self.dismissViewControllerAnimated(true, completion: nil)
     }
-    func didCreateButtonPressed(sender: CreateSinglePlayerView, age:Int, userName:NSString){
+    func didCreateButtonPressed(sender: CreateSinglePlayerView, age:Int, userName:NSString) {
+        view.endEditing(true)
         setStatusBarHidden(false)
         writer.createPlayer(userName as String, age:age)
         self.dismissViewControllerAnimated(true, completion: nil)
