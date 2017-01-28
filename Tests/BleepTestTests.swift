e002d7482ddb2f025fed9ec2a3cf28941bc8584e
Ieuan Tudur Peace <ieuan@semantise.com>
2017-01-28 21:42:14 +0000

    Create the TestLevels

--- Tests/BleepTestTests.swift
+++ Tests/BleepTestTests.swift
@@ -1,6 +1,14 @@
 import XCTest
+import DATAStack
+import CoreData
+@testable import BleepTest
 
 class BleepTestTests: XCTestCase {
+    
+    func createDataStack(storeType: DATAStackStoreType = .InMemory) -> DATAStack {
+        let dataStack = DATAStack(modelName: "iOS", bundle: NSBundle.mainBundle(), storeType:storeType)
+        return dataStack
+    }
     
     override func setUp() {
         super.setUp()
@@ -8,6 +16,26 @@ class BleepTestTests: XCTestCase {
     
     override func tearDown() {
         super.tearDown()
+    }
+    
+    func testBleepTest() {
+        let data = self.createDataStack()
+        
+        data.performInNewBackgroundContext { backgroundContext in
+            let entity = NSEntityDescription.entityForName("TestLevel", inManagedObjectContext: backgroundContext)!
+            let object = NSManagedObject(entity: entity, insertIntoManagedObjectContext: backgroundContext)
+            object.setValue("", forKey:  "id")
+            object.setValue(1, forKey: "level")
+            object.setValue(1, forKey: "laps")
+            object.setValue(10.0, forKey: "lapTime")
+            object.setValue(10.0, forKey: "speed")
+            try! backgroundContext.save()
+        }
+        
+        let request = NSFetchRequest(entityName: "TestLevel")
+        request.sortDescriptors = [NSSortDescriptor(key: "level", ascending: true)]
+        let testLevels = (try! data.mainContext.executeFetchRequest(request) as! [TestLevel])
+
     }
     
 }
