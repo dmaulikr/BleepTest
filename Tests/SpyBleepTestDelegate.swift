3f58ca28cb5950f3623174ad27278da9a497e2a3
Ieuan Tudur Peace <ieuan@semantise.com>
2017-02-14 22:09:06 +0000

    Setting up the Spy Delegate class
    This class is used to test the BleepTest delegates. Using this pattern:
    http://xunitpatterns.com/Test%20Spy.html

---
+++ Tests/SpyBleepTestDelegate.swift
@@ -0,0 +1,23 @@
+import UIKit
+@testable import BleepTest
+
+class SpyBleepTestDelegate : BleepTestDelegate {
+    
+    
+    func lapedUpDelegate(sender: BleepTest, lap: Int, distance: Int, vO2Max: Double) {
+        
+    }
+    
+    func newLevelDelegate(sender: BleepTest, numberOfLaps: Int, level: Int, lapTime: Double) {
+        
+    }
+    
+    func startedNewLap(sender: BleepTest, lap: Int, lapTime: Double) {
+        
+    }
+    
+    func bleepTestFinished(sender: BleepTest) {
+
+    }
+    
+}
