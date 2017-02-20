0daba71e1c8de6bfc0e1d7cf1668170b66d69dec
Ieuan Tudur Peace <ieuan@semantise.com>
2017-02-20 14:37:43 +0000

    Create the Results Table View Controller

---
+++ iOS/Source/ResultsTableController.swift
@@ -0,0 +1,12 @@
+import UIKit
+import CoreData
+import DATAStack
+
+class ResultsTableController: BaseTableViewController {
+ 
+}
+
+extension ResultsTableController {
+    // MARK: - UITableViewDelegate
+    
+}
