b9afef8cc59abd28c37f8bea7ca1125e5981ec14
Ieuan Tudur Peace <ieuan@semantise.com>
2017-02-26 22:23:13 +0000

    Use the ResultsCell

--- iOS/Source/ResultsTableController.swift
+++ iOS/Source/ResultsTableController.swift
@@ -9,8 +9,9 @@ class ResultsTableController: BaseTableViewController {
         let request = NSFetchRequest<NSFetchRequestResult>(entityName: "CompletedTest")
         request.sortDescriptors = [NSSortDescriptor(key: "level", ascending: true)]
         
-        let dataSource = DATASource(tableView: self.tableView, cellIdentifier: "Cell", fetchRequest: request, mainContext: self.dataStack!.mainContext, configuration: { cell, item, indexPath in
+        let dataSource = DATASource(tableView: self.tableView, cellIdentifier:  ResultsCell.Identifier, fetchRequest: request, mainContext: self.dataStack!.mainContext, configuration: { cell, item, indexPath in
             let levelString =  String(format: "Level: %@ - Lap: %@", (item.value(forKey: "level") as? NSNumber)!, (item.value(forKey: "lap") as? NSNumber)!)
+            let cell = cell as! ResultsCell
             cell.textLabel?.text = levelString
         })
         return dataSource
@@ -22,7 +23,7 @@ class ResultsTableController: BaseTableViewController {
     }
     
     func setUpTable() {
-        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        self.tableView.register(ResultsCell.self, forCellReuseIdentifier: ResultsCell.Identifier)
         self.tableView.dataSource = self.dataSource
         self.tableView.tableFooterView = UIView()
         self.tableView.backgroundColor = UIColor.veryLightGrayColor()
