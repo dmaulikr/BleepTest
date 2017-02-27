62f6e9bf8f6703dad0bfba175ef1038864ecb126
Ieuan Tudur Peace <ieuan@semantise.com>
2017-02-27 22:32:44 +0000

    Add the results labels

--- iOS/Source/ResultsCell.swift
+++ iOS/Source/ResultsCell.swift
@@ -4,4 +4,10 @@ class ResultsCell: UITableViewCell {
     
     static let Identifier: String = "ResultsCell"
     
+    var levelLabel = UILabel()
+    var lapLabel = UILabel()
+    var playerNameLabel = UILabel()
+    var vo2MaxLabel = UILabel()
+    var distanceLabel = UILabel()
+
 }
