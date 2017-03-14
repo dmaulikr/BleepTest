788949c5c8d0be007d6bcee828b25425f65d5443
Ieuan Tudur Peace <ieuan@semantise.com>
2017-03-14 17:31:41 +0000

    Set FinishedBleepTestView as the controllers view

--- iOS/Source/FinishedBleepTestController.swift
+++ iOS/Source/FinishedBleepTestController.swift
@@ -2,5 +2,13 @@ import UIKit
 
 class FinishedBleepTestController: BaseViewController {
 
+    lazy var rootView: FinishedBleepTestView = {
+        var temporyView = FinishedBleepTestView(frame: UIScreen.main.bounds)
+        return temporyView
+    }()
+    
+    override func viewDidLoad() {
+        self.view = self.rootView
+    }
 
 }
