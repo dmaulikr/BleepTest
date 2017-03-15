4b984ded853b164e398591a3f142c4d860e073c7
Ieuan Tudur Peace <ieuan@semantise.com>
2017-03-15 14:53:53 +0000

    Use the Initialiser to populate the labels the labels

--- iOS/Source/FinishedBleepTestView.swift
+++ iOS/Source/FinishedBleepTestView.swift
@@ -8,5 +8,24 @@ class FinishedBleepTestView: UIView {
     internal var vo2MaxLabel = UILabel()
     internal var distanceLabel = UILabel()
 
-
+    //MARK: Initializers
+    public init(frame: CGRect, result: Result) {
+        super.init(frame: frame)
+        self.setLabelsValues(result: result)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: Set up view methods
+    
+    private func setLabelsValues(result: Result) {
+        self.levelLabel.text = result.level
+        self.lapLabel.text = result.lap
+        self.playerNameLabel.text = result.playerName
+        self.distanceLabel.text = result.distance
+        self.vo2MaxLabel.text = result.vo2Max
+    }
+    
 }
