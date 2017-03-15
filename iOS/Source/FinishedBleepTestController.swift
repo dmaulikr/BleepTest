271003d262acc46e3e9e46ecd919c1cf169475e4
Ieuan Tudur Peace <ieuan@semantise.com>
2017-03-15 13:56:11 +0000

    Use the Initializer to set up the controller and view

--- iOS/Source/FinishedBleepTestController.swift
+++ iOS/Source/FinishedBleepTestController.swift
@@ -1,14 +1,23 @@
 import UIKit
 
-class FinishedBleepTestController: BaseViewController {
+class FinishedBleepTestController: UIViewController {
 
-    lazy var rootView: FinishedBleepTestView = {
-        var temporyView = FinishedBleepTestView(frame: UIScreen.main.bounds)
+    internal var result: Result
+    
+    lazy internal var rootView: FinishedBleepTestView = {
+        var temporyView = FinishedBleepTestView(frame: UIScreen.main.bounds, result: self.result)
         return temporyView
     }()
     
-    override func viewDidLoad() {
+    // MARK: - Initializers
+    public init(result: Result){
+        self.result = result
+        super.init(nibName: nil, bundle: nil)
         self.view = self.rootView
     }
-
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
