ad4083d4c9e20bca9522b52bc60232ce53757a74
Ieuan Tudur Peace <ieuan@semantise.com>
2017-03-15 14:41:39 +0000

    Add the labels

--- iOS/Source/FinishedBleepTestView.swift
+++ iOS/Source/FinishedBleepTestView.swift
@@ -1,6 +1,12 @@
 import UIKit
 
 class FinishedBleepTestView: UIView {
+    
+    internal var playerNameLabel = UILabel()
+    internal var levelLabel = UILabel()
+    internal var lapLabel = UILabel()
+    internal var vo2MaxLabel = UILabel()
+    internal var distanceLabel = UILabel()
 
 
 }
