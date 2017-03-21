42f01aef6a21da0862b5afe9bacebc723de83d40
Ieuan Tudur Peace <ieuan@semantise.com>
2017-03-21 16:58:35 +0000

    Remove the border

--- Library/Subclasses/Buttons/PinkRedOutlineButton.swift
+++ Library/Subclasses/Buttons/PinkRedOutlineButton.swift
@@ -5,23 +5,9 @@ class PinkRedOutlineButton: UIButton {
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.translatesAutoresizingMaskIntoConstraints = false
-        self.layer.cornerRadius = 8;
-        self.layer.borderWidth = 2;
-        self.layer.borderColor = UIColor.pinkRedColor().cgColor
         self.setTitleColor(UIColor.pinkRedColor(), for: UIControlState())
         self.setTitleColor(UIColor.lightGray, for: UIControlState.highlighted)
         self.backgroundColor = UIColor.clear
-    }
-    
-    override var isHighlighted: Bool {
-        didSet {
-            switch isHighlighted {
-            case true:
-                layer.borderColor = UIColor.lightGray.cgColor
-            case false:
-                layer.borderColor = UIColor.pinkRedColor().cgColor
-            }
-        }
     }
     
     required init?(coder aDecoder: NSCoder) {
