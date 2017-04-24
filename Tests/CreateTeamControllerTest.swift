a9928170195f7b13a79fdefc925ac4d5cffc4cef
Ieuan Tudur Peace <ieuan@semantise.com>
2017-04-24 15:34:15 +0100

    Test view is custom class

--- Tests/CreateTeamControllerTest.swift
+++ Tests/CreateTeamControllerTest.swift
@@ -6,5 +6,6 @@ class CreateTeamControllerTest: XCTestCase {
     func testController() {
         let createTeamController = CreateTeamController()
         XCTAssertNotNil(createTeamController)
+        XCTAssertTrue(createTeamController.view is CreateTeamView)
     }
 }
