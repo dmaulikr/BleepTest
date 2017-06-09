3271d75eaac0784fdc3444b543977bcda00339c7
Ieuan Tudur Peace <ieuan@semantise.com>
2017-06-09 16:34:27 +0100

    Correcting the XCTAssertEqual should not be nil

--- Tests/TestTeam.swift
+++ Tests/TestTeam.swift
@@ -5,13 +5,12 @@ class TestTeam: XCTestCase {
     
     func testInit_ShouldTakeName() {
         let item = Team(name: "Test name")
-        XCTAssertEqual(item.name, "Test name", "Item should not be nil")
+        XCTAssertEqual(item.name, "Test name", "Name item should not be nil")
     }
     
     func testInit_ShouldTakeNameAndDescription() {
         let item = Team(name: "Test name", teamDescription: "This is my team")
-        XCTAssertEqual(item.teamDescription, "This is my team", "Item should not be nil")
-
+        XCTAssertEqual(item.teamDescription, "This is my team", "Team description should not be not nil")
     }
     
 }
