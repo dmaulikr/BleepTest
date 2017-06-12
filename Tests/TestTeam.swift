b329b8e20124f28d5e38abe66fdadfde763e61e0
Ieuan Tudur Peace <ieuan@semantise.com>
2017-06-12 10:36:43 +0100

    Create the players property

--- Tests/TestTeam.swift
+++ Tests/TestTeam.swift
@@ -1,7 +1,14 @@
 import XCTest
+import Sync
+import CoreData
 @testable import BleepTest
 
 class TestTeam: XCTestCase {
+    
+    func createDataStack() -> DataStack {
+        let dataStack = DataStack(modelName: "iOS", bundle: Bundle.main, storeType:.inMemory)
+        return dataStack
+    }
     
     func testInit_ShouldTakeName() {
         let item = Team(name: "Test name")
@@ -11,6 +18,23 @@ class TestTeam: XCTestCase {
     func testInit_ShouldTakeNameAndDescription() {
         let item = Team(name: "Test name", teamDescription: "This is my team")
         XCTAssertEqual(item.teamDescription, "This is my team", "Team description should not be not nil")
+    }
+    
+    func testInit_ShouldTakeNameAndPlayers() {
+        let dataStack = self.createDataStack()
+        
+        let user = NSEntityDescription.insertNewObject(forEntityName: "Player", into: dataStack.mainContext)
+        user.setValue(101, forKey: "age")
+        user.setValue("Billy Jones", forKey: "username")
+        try! dataStack.mainContext.save()
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Player")
+        let objects = try! dataStack.mainContext.fetch(request)
+        
+        
+        let item = Team(name: "Test name", players: objects as? [Player])
+        XCTAssertEqual(item.players?[0].username, "Billy Jones", "Players should not be not nil")
+
     }
     
 }
--- iOS/Models/Team.swift
+++ iOS/Models/Team.swift
@@ -1,10 +1,12 @@
 struct Team {
     
-    init(name: String, teamDescription: String? = nil) {
+    init(name: String, teamDescription: String? = nil, players: [Player]? = nil) {
         self.name = name
         self.teamDescription = teamDescription
+        self.players = players
     }
     
     let name: String
     let teamDescription: String?
+    let players: [Player]?
 }
