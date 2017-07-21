5b9099d4c0313f5f27557322e00b06cd4c1fd7b5
Ieuan Tudur Peace <ieuan@semantise.com>
2017-07-21 16:31:45 +0100

    Populating the view with the test name

--- Tests/DetailTeamViewTests.swift
+++ Tests/DetailTeamViewTests.swift
@@ -1,9 +1,27 @@
 import XCTest
+import Sync
+import CoreData
 @testable import BleepTest
 
 class DetailTeamViewTests: XCTestCase {
     
-    let detailTeamView = DetailTeamView()
+    var detailTeamView: DetailTeamView!
+    var dataStack: DataStack = DataStack()
+    var entity: NSEntityDescription?
+    var team: Team!
+    
+    override func setUp() {
+        super.setUp()
+        self.dataStack = self.createDataStack()
+        self.entity = NSEntityDescription.entity(forEntityName: "Team", in: dataStack.mainContext)
+        team = Team(name: "Test name", entity: self.entity!, insertIntoManagedObjectContext: self.dataStack.mainContext)
+        detailTeamView = DetailTeamView(frame: CGRect(x:0,y:0, width:0, height:0), team)
+    }
+    
+    func createDataStack() -> DataStack {
+        let dataStack = DataStack(modelName: "iOS", bundle: Bundle.main, storeType:.inMemory)
+        return dataStack
+    }
     
     func test_HasTitleLabel() {
         XCTAssertNotNil(detailTeamView.titleLabel)
@@ -11,6 +29,11 @@ class DetailTeamViewTests: XCTestCase {
     
     func test_hasTabelView() {
         XCTAssertNotNil(detailTeamView.playerTabel)
+    }
+    
+    func test_loadViewMethod() {
+        XCTAssertNotNil(detailTeamView.titleLabel.text)
+        XCTAssertEqual(detailTeamView.titleLabel.text!, team.name)
     }
     
 }
--- iOS/Source/DetailTeamView.swift
+++ iOS/Source/DetailTeamView.swift
@@ -5,5 +5,17 @@ class DetailTeamView: UIView {
     let titleLabel = UILabel()
     let playerTabel = UITableView()
     
+    public init(frame: CGRect, _ team: Team) {
+        super.init(frame: frame)
+        self.populateLabels(team)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func populateLabels(_ team: Team) {
+        self.titleLabel.text = team.name
+    }
     
 }
