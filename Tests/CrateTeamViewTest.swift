2c0d3a41e56d413a14e047b84e92b2a555f64ffb
Ieuan Tudur Peace <ieuan@semantise.com>
2017-08-11 19:04:57 +0100

    Correct nameing

--- Tests/CrateTeamViewTest.swift
+++ Tests/CrateTeamViewTest.swift
@@ -10,18 +10,18 @@ class CrateTeamViewTest: XCTestCase {
         createTeamView = CreateTeamView(frame: CGRect(x:0,y:0, width:0, height:0))
     }
     
-    func test_TitleLabel() {
+    func test_titleLabel() {
         XCTAssertNotNil(createTeamView.titleLabel)
         XCTAssertTrue((createTeamView.titleLabel as Any) is MediumBlackLabel)
         XCTAssertEqual(createTeamView.titleLabel.text, "Create team")
     }
     
-    func test_CloseButton() {
+    func test_closeButton() {
         XCTAssertNotNil(createTeamView.closeButton)
         XCTAssertTrue((createTeamView.closeButton as Any) is CloseButton)
     }
     
-    func test_teamNameTextField() {
+    func test_nameTextField() {
         XCTAssertNotNil(createTeamView.nameTextField)
         XCTAssertTrue((createTeamView.nameTextField as Any) is YoshikoTextField)
         XCTAssertEqual(createTeamView.nameTextField.placeholder, "Team Name  e.g. Office Football")
