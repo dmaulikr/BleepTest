84a6327bb9125c1f3e4685e4eba8cf79c2edc53e
Ieuan Tudur Peace <ieuan@semantise.com>
2017-08-11 18:14:00 +0100

    Set up CreateTeamView and its test

--- Tests/CrateTeamViewTest.swift
+++ Tests/CrateTeamViewTest.swift
@@ -1,7 +1,14 @@
 import XCTest
+@testable import BleepTest
 
 class CrateTeamViewTest: XCTestCase {
     
+    var createTeamView : CreateTeamView!
+
+    override func setUp() {
+        super.setUp()
+        createTeamView = CreateTeamView(frame: CGRect(x:0,y:0, width:0, height:0))
+    }
     
 
 }
--- iOS/Source/CreateTeamView.swift
+++ iOS/Source/CreateTeamView.swift
@@ -2,5 +2,13 @@ import UIKit
 
 class CreateTeamView: UIView {
 
-
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
